b66b3d51fa47ccd03e1b05c224bb28600347f26d
Chester <ckraigpc@gmail.com>
2018-05-09 11:45:12 -0700

    Yuson, Ladaga, Tine Min Stack Starting File

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// #Mid Full Stack Project 1
+
+
+
+// #Game Mechanics
+// #fire is weak against  water
+// #electric is strong against water
+// #electric is weak against fire
+
+
+// #User Experiences
+// # Generate Score for user vs computer
+// # Display readable prompt message if user wins or loss
+// # Provide a ui for electric, water, fire
+
+
+// #PROGAM LOGIC
+// # Create functions (win,loss, draw,getComputerChoice game,main)
+// # Identify these functions which get a parameter or argument.
+// # Implements listener event everytime the user picks among the elements
+// # 
+
+// #get all the elements in the DOM and undefined variables
+
+
+
+// #game logic for getComputerChoice()
+
+
+
+// #game logic for win()
+
+
+
+
+// #game logic for loose()
+
+
+
+
+// #game logic for draw()
+
+
+
+
+// #logic for the whole game()
+
+
+
+
+// #run main()
+
